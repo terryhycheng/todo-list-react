a5d08a6545d1525f36f807728622992cf682926c
terryhycheng <terryhycheng@gmail.com>
2023-01-22 00:46:36 +0000

    Initial setup with TailwindCSS, Eslint, Prettier and testing libraries

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:cypress/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'cypress'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'prettier/prettier': 'error',
+  },
+};
---
+++ src/App.test.tsx
@@ -0,0 +1,17 @@
+import { describe, it, expect } from 'vitest';
+import { screen, render } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('renders Hello World!', () => {
+    // ARRANGE
+    render(<App />);
+
+    // ACTION
+
+    // EXPECT
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Hello World!'
+    );
+  });
+});
